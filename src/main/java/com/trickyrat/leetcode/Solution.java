5fe473c7c1215154ffda95dc509971028fa800dd
trickyrat <wangjian0513@outlook.com>
2022-02-25 22:25:30 +0800

    添加537.复数的乘法

--- src/main/java/com/trickyrat/leetcode/Solution.java
+++ src/main/java/com/trickyrat/leetcode/Solution.java
@@ -206,6 +206,23 @@ public class Solution {
         return segmentCount;
     }
 
+    /**
+     * 537. 复数的乘法
+     * 
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String complexNumberMultiply(String num1, String num2) {
+        String[] complex1 = num1.split("\\+|i");
+        String[] complex2 = num2.split("\\+|i");
+        int real1 = Integer.parseInt(complex1[0]);
+        int real2 = Integer.parseInt(complex1[0]);
+        int imag1 = Integer.parseInt(complex1[1]);
+        int imag2 = Integer.parseInt(complex2[1]);
+        return String.format("%d+%di", real1 * real2 - imag2 * imag1, real1 * imag2 + imag1 * real2);
+    }
+
     /*
      * 807.Max Increase to Keep City Skyline
      */
