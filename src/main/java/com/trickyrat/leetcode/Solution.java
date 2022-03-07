c0a33757d6928bb7458685d459e3bf3cd0b91213
trickyrat <wangjian0513@outlook.com>
2022-03-07 20:44:06 +0800

    添加504.七进制数

--- src/main/java/com/trickyrat/leetcode/Solution.java
+++ src/main/java/com/trickyrat/leetcode/Solution.java
@@ -282,6 +282,29 @@ public class Solution {
     }
 
     /**
+     * 504.七进制数
+     * 
+     * @param num
+     * @return
+     */
+    public String convertToBase7(Integer num) {
+        if (num == 0) {
+            return "0";
+        }
+        boolean negative = num < 0;
+        num = Math.abs(num);
+        StringBuffer digits = new StringBuffer();
+        while (num > 0) {
+            digits.append(num % 7);
+            num /= 7;
+        }
+        if (negative) {
+            digits.append('-');
+        }
+        return digits.reverse().toString();
+    }
+
+    /**
      * 521.最长特殊序列
      * 
      * @param a
