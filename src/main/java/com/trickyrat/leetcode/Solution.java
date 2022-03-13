45368ac5641117b4f891c6abb71fdded77c1d958
trickyrat <wangjian0513@outlook.com>
2022-03-13 15:16:08 +0800

    添加393.UTF-8编码验证

--- src/main/java/com/trickyrat/leetcode/Solution.java
+++ src/main/java/com/trickyrat/leetcode/Solution.java
@@ -266,6 +266,33 @@ public class Solution {
     }
 
     /**
+     * 393. UTF-8编码验证
+     *
+     * @param data
+     * @return
+     */
+    public boolean validUtf8(int[] data) {
+        int n = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (n > 0) {
+                if (data[i] >> 6 != 2) return false;
+                n--;
+            } else if (data[i] >> 7 == 0) {
+                n = 0;
+            } else if (data[i] >> 5 == 0b110) {
+                n = 1;
+            } else if (data[i] >> 4 == 0b1110) {
+                n = 2;
+            } else if (data[i] >> 3 == 0b11110) {
+                n = 3;
+            } else {
+                return false;
+            }
+        }
+        return n == 0;
+    }
+
+    /**
      * 434. Number of Segments in a String
      * 
      * @param s
