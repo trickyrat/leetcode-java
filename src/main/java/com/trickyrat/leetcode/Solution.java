212ca21e552e5c7b1d3cf4da8b6d78e0c63775a3
trickyrat <wangjian0513@outlook.com>
2022-04-11 01:08:39 +0800

    添加357. 统计各位数字都不同的数字个数

--- src/main/java/com/trickyrat/leetcode/Solution.java
+++ src/main/java/com/trickyrat/leetcode/Solution.java
@@ -287,6 +287,26 @@ public class Solution {
     }
 
     /**
+     * 357. 统计各位数字都不同的数字个数
+     * @param n
+     * @return
+     */
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int res = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++) {
+            cur *= 9 - i;
+            res += cur;
+        }
+        return res;
+    }
+
+    /**
      * 393. UTF-8编码验证
      *
      * @param data
