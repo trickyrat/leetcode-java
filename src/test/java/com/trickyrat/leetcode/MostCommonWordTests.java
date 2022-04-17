bb546e31f75409e9b1d14af21a078e075bd6672d
trickyrat <wangjian0513@outlook.com>
2022-04-17 23:03:27 +0800

    添加819. 最常见的单词

---
+++ src/test/java/com/trickyrat/leetcode/MostCommonWordTests.java
@@ -0,0 +1,24 @@
+package com.trickyrat.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MostCommonWordTests {
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("getData")
+    public void test(String words, String[] banned, String expect) {
+        String actual = solution.mostCommonWord(words, banned);
+        Assertions.assertEquals(expect, actual);
+    }
+
+    static Stream<Arguments> getData() {
+        return Stream.of(
+                Arguments.arguments("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"}, "ball"));
+    }
+}
