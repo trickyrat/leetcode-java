50e3741cd43ff948b78cee52543b08b32c116557
trickyrat <wangjian0513@outlook.com>
2022-04-28 23:50:21 +0800

    添加905. 按奇偶排序数组

--- src/main/java/com/trickyrat/leetcode/Solution.java
+++ src/main/java/com/trickyrat/leetcode/Solution.java
@@ -757,6 +757,31 @@ public class Solution {
     }
 
     /**
+     * 905. 按奇偶排序数组
+     * @param nums
+     * @return
+     */
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            while(left < right && nums[left] % 2 == 0) {
+                left++;
+            }
+            while(left < right && nums[right] % 2 == 1) {
+                right++;
+            }
+            if(left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return nums;
+    }
+
+    /**
      * 1380. 矩阵中的幸运数字
      *
      * @param matrix
