1c7379967b732ee61dfc2b65abbc5caf0b626747
trickyrat <wangjian0513@outlook.com>
2022-08-24 18:12:47 +0800

    add some testcases

--- src/main/java/org/trickyrat/ListNode.java
+++ src/main/java/org/trickyrat/ListNode.java
@@ -1,5 +1,8 @@
 package org.trickyrat;
 
+/**
+ * Definition for singly-linked list.
+ */
 public class ListNode {
   int val;
   ListNode next;
@@ -20,5 +23,19 @@ public class ListNode {
   public boolean equals(Object obj) {
     ListNode clone = (ListNode)obj;
     return this.val == clone.val;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder ret = new StringBuilder();
+    ListNode dummy = this;
+    while (dummy != null) {
+      ret.append(dummy.val);
+      if (dummy.next == null) {
+        ret.append("->");
+      }
+      dummy = dummy.next;
+    }
+    return ret.toString();
   }
 }
--- src/main/java/org/trickyrat/Utilities.java
+++ src/main/java/org/trickyrat/Utilities.java
@@ -6,79 +6,70 @@ import java.util.List;
 import java.util.Queue;
 
 public class Utilities {
-
-  public static ListNode createListNode(int[] nums) {
-    ListNode head = new ListNode(0);
-    ListNode dummyHead = head;
-    for(int num : nums) {
-      dummyHead.next = new ListNode(num);
-      dummyHead = dummyHead.next;
-    }
-    return head.next;
-  }
-
-  public static String ListNodeToString(ListNode head) {
-    StringBuilder ret = new StringBuilder();
-    while(head != null) {
-      if(head.next == null) {
-        ret.append(head.val);
-      } else {
-        ret.append(head.val).append("->");
-      }
-      head = head.next;
-    }
-    return ret.toString();
-  }
-
-  public static TreeNode createTreeNodeWithBFS(String data) {
-    String[] nums = data.split(",");
-    if (nums[0].equals("null")) {
-      return null;
-    }
-    TreeNode root = new TreeNode(Integer.parseInt(nums[0]));
-    Queue<TreeNode> queue = new LinkedList<TreeNode>();
-    queue.add(root);
-    int index = 1;
-    while (index < nums.length) {
-      TreeNode node = queue.poll();
-      if(index > nums.length - 1 || nums[index].equals("null")) {
-        node.left = null;
-      } else {
-        TreeNode leftNode = new TreeNode(Integer.parseInt(nums[index]));
-        if (node != null) {
-          node.left = leftNode;
+    /**
+     * Create a ListNode by array.
+     * @param nums input array
+     * @return the head node
+     */
+    public static ListNode createListNode(int[] nums) {
+        ListNode head = new ListNode(0);
+        ListNode dummyHead = head;
+        for (int num : nums) {
+            dummyHead.next = new ListNode(num);
+            dummyHead = dummyHead.next;
         }
-        queue.add(leftNode);
-      }
-      if(index + 1 > nums.length - 1 || nums[index+1].equals("null")) {
-        node.right = null;
-      } else {
-        TreeNode rightNode = new TreeNode(Integer.parseInt(nums[index+1]));
-        if (node != null) {
-          node.right = rightNode;
+        return head.next;
+    }
+
+    public static TreeNode createTreeNodeWithBFS(String data) {
+        String[] nums = data.split(",");
+        if (nums[0].equals("null")) {
+            return null;
         }
-        queue.add(rightNode);
-      }
-      index += 2;
+        TreeNode root = new TreeNode(Integer.parseInt(nums[0]));
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int index = 1;
+        while (index < nums.length) {
+            TreeNode node = queue.poll();
+            if (index > nums.length - 1 || nums[index].equals("null")) {
+                node.left = null;
+            } else {
+                TreeNode leftNode = new TreeNode(Integer.parseInt(nums[index]));
+                if (node != null) {
+                    node.left = leftNode;
+                }
+                queue.add(leftNode);
+            }
+            if (index + 1 > nums.length - 1 || nums[index + 1].equals("null")) {
+                node.right = null;
+            } else {
+                TreeNode rightNode = new TreeNode(Integer.parseInt(nums[index + 1]));
+                if (node != null) {
+                    node.right = rightNode;
+                }
+                queue.add(rightNode);
+            }
+            index += 2;
+        }
+        return root;
     }
-    return root;
-  }
 
-  public static TreeNode createTreeNodeWithDFS(String data) {
-    String[] dataArray = data.split(",");
-    List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));
-    return dfs(dataList);
-  }
-
-  private static TreeNode dfs(List<String> dataList) {
-    if (dataList.get(0).equals("null")) {
-      dataList.remove(0);
-      return null;
+    public static TreeNode createTreeNodeWithDFS(String data) {
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));
+        return dfs(dataList);
     }
-    TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
-    dataList.remove(0);
-    root.left = dfs(dataList);
-    root.right = dfs(dataList);
-    return root;
-  }
+
+    private static TreeNode dfs(List<String> dataList) {
+        if (dataList.get(0).equals("null")) {
+            dataList.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        root.left = dfs(dataList);
+        root.right = dfs(dataList);
+        return root;
+    }
 }
