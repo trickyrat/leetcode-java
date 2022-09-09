d92f03c99a4fbc270d3cb094f980a8249a25c7e2
trickyrat <wangjian0513@outlook.com>
2022-09-10 00:43:17 +0800

    add 667. Beautiful Arrangement II

--- src/main/java/org/trickyrat/Solution.java
+++ src/main/java/org/trickyrat/Solution.java
@@ -730,6 +730,27 @@ public class Solution {
     }
 
     /**
+     * 667. Beautiful Arrangement II
+     * @param n
+     * @param k
+     * @return
+     */
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        int index =0;
+        for (int i = 1; i < n - k; i++) {
+            res[index++] = i;
+        }
+        for (int i = n - k, j = n; i <= j; i++, j--) {
+            res[index++] = i;
+            if(i != j) {
+                res[index++] = j;
+            }
+        }
+        return res;
+    }
+
+    /**
      * 682. Baseball Game
      *
      * @param ops
