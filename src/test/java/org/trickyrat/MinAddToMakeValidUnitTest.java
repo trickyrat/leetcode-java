58b282da6c5709843b4638cbd8ca79b3a9e606ef
trickyrat <wangjian0513@outlook.com>
2022-10-07 15:47:53 +0800

    test: add minAddToMakeValid test cases

---
+++ src/test/java/org/trickyrat/MinAddToMakeValidUnitTest.java
@@ -0,0 +1,26 @@
+package org.trickyrat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MinAddToMakeValidUnitTest {
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("getData")
+    public void test(String s, int expected) {
+        var actual = solution.minAddToMakeValid(s);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> getData() {
+        return Stream.of(
+                Arguments.arguments("())", 1),
+                Arguments.arguments("(((", 3)
+        );
+    }
+}
