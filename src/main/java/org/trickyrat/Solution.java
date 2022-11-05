295076bf7ee05ac6c163c12a3fe8625ff640ba7c
trickyrat <wangjian0513@outlook.com>
2022-11-06 00:36:59 +0800

    feature: add 1700. Number of Students Unable to Eat Lunch

--- src/main/java/org/trickyrat/Solution.java
+++ src/main/java/org/trickyrat/Solution.java
@@ -2137,6 +2137,27 @@ public class Solution {
     }
 
     /**
+     *
+     * @param students
+     * @param sandwiches
+     * @return
+     */
+    public int countStudents(int[] students, int[] sandwiches) {
+        int s1 = Arrays.stream(students).sum();
+        int s0 = students.length - s1;
+        for (int i = 0; i < sandwiches.length; i++) {
+            if(sandwiches[i] == 0 && s0 > 0) {
+                s0--;
+            } else if (sandwiches[i] == 1 && s1 > 0) {
+                s1--;
+            } else {
+                break;
+            }
+        }
+        return s0 + s1;
+    }
+
+    /**
      * 1784. Check if Binary String Has at Most One Segment of Ones
      *
      * @param s
