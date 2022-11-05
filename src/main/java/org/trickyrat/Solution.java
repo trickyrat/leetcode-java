04aac3a337782a2a3c3e660121e83f1df635de21
trickyrat <wangjian0513@outlook.com>
2022-11-06 00:10:24 +0800

    feature: add 886. Possible Bipartition

--- src/main/java/org/trickyrat/Solution.java
+++ src/main/java/org/trickyrat/Solution.java
@@ -1395,6 +1395,44 @@ public class Solution {
     }
 
     /**
+     * 886. Possible Bipartition
+     * @param n
+     * @return
+     */
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        int[] color = new int[n + 1];
+        List<Integer>[] g = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            g[i] = new ArrayList<>();
+        }
+        for (int[] p : dislikes) {
+            g[p[0]].add(p[1]);
+            g[p[1]].add(p[0]);
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            if (color[i] == 0) {
+                Queue<Integer> queue = new ArrayDeque<>();
+                queue.offer(i);
+                color[i] = 1;
+                while (!queue.isEmpty()) {
+                    int t = queue.poll();
+                    for (int next : g[t]) {
+                        if (color[next] > 0 && color[next] == color[t]) {
+                            return false;
+                        }
+                        if (color[next] == 0) {
+                            color[next] = 3 ^ color[t];
+                            queue.offer(next);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * 905. Sort Array By Parity
      *
      * @param nums
