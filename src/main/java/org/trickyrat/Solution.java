e76788f6079c597bd674c030ecbd75ea9b3d02a9
trickyrat <wangjian0513@outlook.com>
2023-01-15 16:21:03 +0800

    feat: add 19. Remove Nth Node From End of List

--- src/main/java/org/trickyrat/Solution.java
+++ src/main/java/org/trickyrat/Solution.java
@@ -147,6 +147,26 @@ public class Solution {
     }
 
     /**
+     * 19. Remove Nth Node From End of List
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode fast = head, slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+    /**
      * 23. Merge K Sorted Lists
      * @param lists
      * @return
---
+++ src/test/java/org/trickyrat/RemoveNthFromEndUnitTest.java
@@ -0,0 +1,27 @@
+package org.trickyrat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class RemoveNthFromEndUnitTest {
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("getData")
+    public void test(ListNode head, int n, ListNode expected) {
+        ListNode actual = solution.removeNthFromEnd(head, n);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> getData() {
+        return Stream.of(
+                Arguments.arguments(Utilities.createListNode(new int[]{1, 2, 3, 4, 5}), 2, Utilities.createListNode(new int[]{1, 2, 4, 5})),
+                Arguments.arguments(Utilities.createListNode(new int[]{1}), 1, null),
+                Arguments.arguments(Utilities.createListNode(new int[]{1, 2}), 1, Utilities.createListNode(new int[]{1}))
+        );
+    }
+}
