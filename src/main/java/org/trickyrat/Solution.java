d8b0a63e3d047e50df0daac246a4a4e5f81e70c2
trickyrat <wangjian0513@outlook.com>
2023-03-19 01:43:50 +0800

    feat: add 26. Remove Duplicates from Sorted Array

--- src/main/java/org/trickyrat/Solution.java
+++ src/main/java/org/trickyrat/Solution.java
@@ -224,6 +224,27 @@ public class Solution {
     }
 
     /**
+     * 26. Remove Duplicates from Sorted Array
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    /**
      * 38. Count And Say
      *
      * @param n
