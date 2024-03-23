14b46fd6af6e790e9c67b32991cfd7787b0ccd58
trickyrat <wangjian0513@outlook.com>
2024-03-23 11:37:57 +0800

    chore: update ListNode

--- src/main/java/org/trickyrat/ListNode.java
+++ src/main/java/org/trickyrat/ListNode.java
@@ -18,29 +18,4 @@ public class ListNode {
     this.val = val;
     this.next = next;
   }
-
-  @Override
-  public boolean equals(Object obj) {
-    ListNode dummy1 = (ListNode)obj;
-    ListNode dummy2 = this;
-    while (dummy1 != null && dummy2 != null && dummy1.val == dummy2.val) {
-      dummy1 = dummy1.next;
-      dummy2 = dummy2.next;
-    }
-    return dummy1 == null && dummy2 == null;
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder ret = new StringBuilder();
-    ListNode dummy = this;
-    while (dummy != null) {
-      ret.append(dummy.val);
-      if (dummy.next != null) {
-        ret.append("->");
-      }
-      dummy = dummy.next;
-    }
-    return ret.toString();
-  }
 }
