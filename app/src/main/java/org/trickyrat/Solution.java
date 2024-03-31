e59a9f2871373dd7b6e3b656960c62b765a7af9e
trickyrat <wangjian0513@outlook.com>
2024-03-31 16:40:46 +0800

    feat: Add 331. Verify Preorder Serialization of a Binary Tree

--- app/src/main/java/org/trickyrat/Solution.java
+++ app/src/main/java/org/trickyrat/Solution.java
@@ -514,6 +514,34 @@ public class Solution {
     }
 
     /**
+     * 331. Verify Preorder Serialization of a Binary Tree
+     * @param preorder
+     * @return
+     */
+    public boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        int slots = 1;
+        while (i < n) {
+            if (slots == 0) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#'){
+                slots--;
+                i++;
+            } else {
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                slots++;
+            }
+        }
+        return slots == 0;
+    }
+
+    /**
      * 357. Count Numbers with Unique Digits
      *
      * @param n
